8db4cde4deee26b6a5a5e4f84b490fbbdeafd158
ELÍAS MUÑÓZ STEMPHELET <56279790@ceibal.edu.uy>
2024-05-28 18:54:40 -0300

    Ejercicios_condicionales

---
+++ actividad.php
@@ -0,0 +1,39 @@
+<?php 
+/*
+$nota = 90;
+echo"La calificacion es $nota<br>";
+
+if($nota >= 90){
+    echo"La calificacion es A";
+
+}
+elseif($nota>=80){
+    echo"La calificacion es B";
+
+}
+elseif($nota>=70){
+    echo"La calificacion es C";
+
+}
+if($nota<70){
+    echo"La calificacion es F";
+
+}
+*/
+
+/*
+$hora = 14;
+
+if($hora<=12){
+    echo"Buenos Dias";
+}
+elseif($hora>6){
+    echo"Buenas Tardes";
+}
+else {
+    echo"Buenas Noches";
+}
+*/  
+?>
+
+
